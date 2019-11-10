d90dd57e9109f3277f690f4dc0f4831e69a01025
arthas168 <pavlovskipetko@abv.bg>
2019-11-10 21:57:17 +0200

    implemented UserInfo component and page

--- src/AtomicSwap.js
+++ src/AtomicSwap.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SwapState from './components/Swap/swapState';
 import HomePage from './components/Home';
 import Swap from './components/Swap';
+import UserInfo from './components/UserInfo';
 import LoginView from './components/Login';
 import RegisterView from './components/Register';
 import About from './components/About';
@@ -14,6 +15,7 @@ function AtomicSwap() {
 		<SwapState>
 			<HomePage />
 			<Swap />
+			<UserInfo />
 			<LoginView />
 			<RegisterView />
 			<About />
--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,36 +1,84 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 export default function UserInfo() {
-  return (
-    <section id="login">
-      <div className="container">
-          <p>Portfolio Value: 20045$</p>
-        <div className="transaction-list">
-          <div className="transaction-card">
-            <p>
-              <span>Exchanged: X ETH for Y TRX</span>
-              <span>On date: 21-11-2019</span>
-              <Button variant="primary">More Info</Button>
-            </p>
-          </div>
-          <div className="transaction-card">
-            <p>
-              <span>Exchanged: X ETH for Y TRX</span>
-              <span>On date: 21-11-2019</span>
-              <Button variant="primary">More Info</Button>
-            </p>
-          </div>
-          <div className="transaction-card">
-            <p>
-              <span>Exchanged: X ETH for Y TRX</span>
-              <span>On date: 21-11-2019</span>
-              <Button variant="primary">More Info</Button>
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section id="userInfo">
+			<div className="container">
+				<div className="main-info">
+					<h1>Total Portfolio Value: 20045$</h1>
+					<div className="currencies-list">
+						<div className="eth">
+							<p>ETH (ETHEREUM) - 2.5</p>
+							<p>786$</p>
+						</div>
+						<div className="trx">
+							<p>TRON (TRX) - 12243</p>
+							<p>20$</p>
+						</div>
+					</div>
+				</div>
+				<div className="transaction-list">
+          <p className="history">Transaction History</p>
+					<Scrollbars className="scroller-area" style={{ width: 400, height: 525 }}>
+						<div className="transaction-card">
+							<p>Exchanged: X ETH for Y TRX</p>
+							<p>On date: 21-11-2019</p>
+
+							<Button variant="primary">More Info</Button>
+						</div>
+						<div className="transaction-card">
+							<p>Exchanged: X ETH for Y TRX</p>
+							<p>On date: 21-11-2019</p>
+
+							<Button variant="primary">More Info</Button>
+						</div>
+						<div className="transaction-card">
+							<p>Exchanged: X ETH for Y TRX</p>
+							<p>On date: 21-11-2019</p>
+
+							<Button variant="primary">More Info</Button>
+						</div>
+						<div className="transaction-card">
+							<p>Exchanged: X ETH for Y TRX</p>
+							<p>On date: 21-11-2019</p>
+
+							<Button variant="primary">More Info</Button>
+						</div>
+						<div className="transaction-card">
+							<p>Exchanged: X ETH for Y TRX</p>
+							<p>On date: 21-11-2019</p>
+
+							<Button variant="primary">More Info</Button>
+						</div>
+						<div className="transaction-card">
+							<p>Exchanged: X ETH for Y TRX</p>
+							<p>On date: 21-11-2019</p>
+
+							<Button variant="primary">More Info</Button>
+						</div>
+						<div className="transaction-card">
+							<p>Exchanged: X ETH for Y TRX</p>
+							<p>On date: 21-11-2019</p>
+
+							<Button variant="primary">More Info</Button>
+						</div>
+						<div className="transaction-card">
+							<p>Exchanged: X ETH for Y TRX</p>
+							<p>On date: 21-11-2019</p>
+
+							<Button variant="primary">More Info</Button>
+						</div>
+						<div className="transaction-card">
+							<p>Exchanged: X ETH for Y TRX</p>
+							<p>On date: 21-11-2019</p>
+							<Button variant="primary">More Info</Button>
+						</div>
+					</Scrollbars>
+				</div>
+			</div>
+		</section>
+	);
 }
